8ca561c35809787720a43496e06b81ef3d5c257b
Bar <the.bar.yehudai@gmail.com>
2018-10-20 19:37:01 +0300

    Updates

---
+++ precache-manifest.8cd3535819e71d89c7a017db78007a16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2fe2bf076e4299bc1c7",
+    "url": "/HeroloCinema/public/static/js/runtime~main.d2fe2bf0.js"
+  },
+  {
+    "revision": "1f5867d32d7de7c791b5",
+    "url": "/HeroloCinema/public/static/js/main.1f5867d3.chunk.js"
+  },
+  {
+    "revision": "8252f3843cf81deef9cd",
+    "url": "/HeroloCinema/public/static/js/1.8252f384.chunk.js"
+  },
+  {
+    "revision": "1f5867d32d7de7c791b5",
+    "url": "/HeroloCinema/public/static/css/main.bb4d434c.chunk.css"
+  },
+  {
+    "revision": "5bd10ecc4a4deba46b339f10a3147ead",
+    "url": "/HeroloCinema/public/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/HeroloCinema/precache-manifest.1bd90453a80baabcb6d8259014f023f9.js"
+  "/HeroloCinema/public/precache-manifest.8cd3535819e71d89c7a017db78007a16.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/HeroloCinema/index.html", {
+workbox.routing.registerNavigationRoute("/HeroloCinema/public/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

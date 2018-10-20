44b7791752cf198d27f59113b9c6d797c7b28063
Bar <the.bar.yehudai@gmail.com>
2018-10-20 20:28:56 +0300

    change links to https

--- src/actions/fetchAction.js
+++ src/actions/fetchAction.js
@@ -3,7 +3,7 @@ import { addMovieAction } from './addMovieAction'
 
 const fetchMovie = async (dispatch, title) => {
     console.log('fetching movie: ' + title)
-    var fetchstr = 'http://www.omdbapi.com/?apikey=4834ebc9&t=' + title.replace(' ', '+')
+    var fetchstr = 'https://www.omdbapi.com/?apikey=4834ebc9&t=' + title.replace(' ', '+')
     var movres = await fetch(fetchstr)
     var movdata = await movres.json()
     addMovieAction(movdata)(dispatch)
@@ -11,7 +11,7 @@ const fetchMovie = async (dispatch, title) => {
 
 export const fetchMovies = async (dispatch, searchword) => {
     console.log('fetchMovies')
-    var res = await fetch('http://www.omdbapi.com/?apikey=4834ebc9&s=' + searchword.replace(' ', '+'))
+    var res = await fetch('https://www.omdbapi.com/?apikey=4834ebc9&s=' + searchword.replace(' ', '+'))
     var data = await res.json()
     toggleLoading()(dispatch)
     data.Search.forEach((mov) => {
